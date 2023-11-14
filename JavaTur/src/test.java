a174287f0b452846a0da9f17d3eed1d3d54644c5
Frouen M. Medina Jr <117834496+WenDEVLIFE@users.noreply.github.com>
2023-11-14 20:00:25 +0800

    push
    push

---
+++ JavaTur/src/test.java
@@ -0,0 +1,26 @@
+
+/* this is a multi-line comments */
+//this is the class name 
+public class test {
+// main method sa program kaning public static void
+public static void main(String [] args) {
+// basta naa sa sulod sa maim method dira nato i excecute 
+	
+	// this is the datatypes and variable
+	
+		String myname = "Frouen";
+		int age = 20;
+		float percentage =99.99f;
+		double value = 9999.999;
+		boolean hello= true;
+	
+	System.out.println("Hello my name is " + myname);
+	System.out.println("My age is " + age);
+	System.out.println("My Programming skills is " + percentage);
+	System.out.println("My Cooking skill is " + value);
+	System.out.println("I am single true or false?, the answer is: " + hello);
+	
+}
+
+
+}
